843ad016888156808417f38bf3f418ca6386498e
Peter <59874288+Peter-Easterbrook@users.noreply.github.com>
2022-11-01 13:05:10 +0100

    Update script
    Remove deprecated execComand() function

--- script.js
+++ script.js
@@ -15,20 +15,13 @@ const randomFunc = {
 };
 
 clipboardEl.addEventListener('click', () => {
-  const textarea = document.createElement('textarea');
-  const password = resultEl.innerText;
-
+    const password = resultEl.innerText;
   if (!password) {
     return;
   }
-
-  textarea.value = password;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand('copy');
-  textarea.remove();
-  alert('Password copied to clipboard!');
-});
+  navigator.clipboard.writeText(password);
+    alert('Password copied to clipboard!')
+})
 
 generateEl.addEventListener('click', () => {
   const length = +lengthEl.value;
